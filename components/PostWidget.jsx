b89f15b25754a704706600b535cd9c5b23f8ac3f
Darwin <lynasty@gmail.com>
2022-01-13 22:35:08 +0100

    v1

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -21,27 +21,29 @@ const PostWidget = ({ categories, slug }) => {
         {slug ? "Related Posts" : "Recent Posts"}
       </h3>
       {relatedPosts.map((post) => (
-        <>
-          <div key={post.title} className="flex items-center w-full mb-4">
-            <div className="w-16 flex-none">
-              <img
-                src={post.featuredImage.url}
-                alt={post.title}
-                height="60px"
-                weight="60px"
-                className="align-middle rounded-full"
-              />
-            </div>
-            <div className="flex-grow ml-4">
-              <p className="text-gray-500 font-xs">
-                {moment(post.createdAt).format('MMM DD, YYYY')}
-              </p>
-              <Link href={`/post/${post.slug}`} key={post.title} className="text-md">
-                {post.title}
-              </Link>
-            </div>
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              height="60px"
+              weight="60px"
+              className="align-middle rounded-full"
+            />
           </div>
-        </>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              key={post.title}
+              className="text-md"
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
       ))}
     </div>
   );
